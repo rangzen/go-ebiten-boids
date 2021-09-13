f4d69c70cc0265f47deeed7abc70ef461fd6cca3
Cédric L’homme <public@l-homme.com>
2021-09-12 23:40:58 -0400

    add cohesion and separation rules

--- rules.go
+++ rules.go
@@ -12,7 +12,14 @@ func NewDefaultRuler() Ruler {
 		neighborDistance: 50,
 		factor:           10,
 	}
-	rules = append(rules, ar)
+	cr := CohesionRule{
+		factor: .01,
+	}
+	sr := SeparationRule{
+		neighborDistance: 50,
+		factor:           .01,
+	}
+	rules = append(rules, ar, cr, sr)
 	return rules
 }
 
@@ -42,5 +49,55 @@ func (a AlignmentRule) Apply(flock Flock, index int) Vector {
 	origV := flock[index].velocity
 	v.X = (v.X - origV.X) * a.factor / 100
 	v.Y = (v.Y - origV.Y) * a.factor / 100
+	return v
+}
+
+type CohesionRule struct {
+	factor float64
+}
+
+func (c CohesionRule) Apply(flock Flock, index int) Vector {
+	v := Vector{}
+	nbBoid := 0
+	for i, boid := range flock {
+		if i == index {
+			continue
+		}
+		v.add(boid.position)
+		nbBoid++
+	}
+	if nbBoid == 0 {
+		return v
+	}
+	v.X = v.X / float64(nbBoid)
+	v.Y = v.Y / float64(nbBoid)
+
+	origV := flock[index].position
+	v.X = (v.X - origV.X) * c.factor / 100
+	v.Y = (v.Y - origV.Y) * c.factor / 100
+	return v
+}
+
+type SeparationRule struct {
+	neighborDistance float64
+	factor           float64
+}
+
+func (s SeparationRule) Apply(flock Flock, index int) Vector {
+	v := Vector{}
+	origP := flock[index].position
+	for i, boid := range flock {
+		if i == index {
+			continue
+		}
+		if flock[index].IsNeighbor(*boid, s.neighborDistance) {
+			v.sub(Vector{
+				X: boid.position.X - origP.X,
+				Y: boid.position.Y - origP.Y,
+			})
+		}
+	}
+	v.X *= s.factor / 100
+	v.Y *= s.factor / 100
 	return v
 }
--- vector.go
+++ vector.go
@@ -8,3 +8,8 @@ func (v *Vector) add(v2 Vector) {
 	v.X += v2.X
 	v.Y += v2.Y
 }
+
+func (v *Vector) sub(v2 Vector) {
+	v.X -= v2.X
+	v.Y -= v2.Y
+}
